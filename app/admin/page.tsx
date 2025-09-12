b7bc10eb0bb22e473a3fbdf2340a80aaa38bcaad
Blackicon <mattiaverdecchi@yahoo.it>
2025-09-12 14:52:32 +0200

    Added login page texts

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,7 @@
 import { SignInWithBaseButton } from "@base-org/account-ui/react";
 import { Loader2 } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
+import Image from "next/image";
 import { useState } from "react";
 import { AdminBanner } from "@/components/custom-ui/admin/admin-banner";
 import { BrandContent } from "@/components/custom-ui/admin/brand/brand-content";
@@ -33,50 +34,7 @@ export default function AdminPage() {
           className="flex justify-center items-center w-full min-h-screen">
           <Loader2 className="size-14 animate-spin" />
         </motion.div>
-      ) : brand.brandNotFound ? (
-        <motion.div
-          key="brand-not-found"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.25, ease: "easeInOut" }}
-          className="flex justify-center items-center w-full min-h-screen">
-          <div className="flex flex-col items-center justify-center w-full px-10 gap-7">
-            <p className="text-3xl font-bold text-center">
-              We could not find your brand
-              <br />
-              <span className="text-lg opacity-50 -mt-2">
-                Please try again with a different account
-              </span>
-            </p>
-            <div className="w-fit">
-              <SignInWithBaseButton
-                align="center"
-                variant="solid"
-                colorScheme="light"
-                onClick={signInWithBase}
-              />
-            </div>
-          </div>
-        </motion.div>
-      ) : !brand.data ? (
-        <motion.div
-          key="sign-in-with-base"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.25, ease: "easeInOut" }}
-          className="flex justify-center items-center w-full min-h-screen">
-          <div className="w-fit">
-            <SignInWithBaseButton
-              align="center"
-              variant="solid"
-              colorScheme="light"
-              onClick={signInWithBase}
-            />
-          </div>
-        </motion.div>
-      ) : (
+      ) : !!brand.data ? (
         <motion.div
           key="admin-page"
           initial={{ opacity: 0 }}
@@ -104,6 +62,66 @@ export default function AdminPage() {
                 <PluginsContent key="plugins" />
               )}
             </AnimatePresence>
+          </div>
+        </motion.div>
+      ) : (
+        <motion.div
+          key="sign-in-with-base"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.25, ease: "easeInOut" }}
+          className="flex justify-center items-center w-full min-h-screen">
+          <div className="flex flex-col items-center justify-center w-full px-10 gap-7">
+            <div className="flex flex-col items-center justify-center w-full gap-4">
+              <Image
+                src="/images/rollup_logo_black.png"
+                alt="The Rollup logo"
+                width={560}
+                height={100}
+                priority
+              />
+              <AnimatePresence mode="wait">
+                {brand.brandNotFound ? (
+                  <motion.p
+                    key="brand-not-found"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.25, ease: "easeInOut" }}
+                    className="text-4xl font-bold text-center">
+                    We could not find your brand
+                    <br />
+                    <span className="text-lg opacity-50 -mt-2">
+                      Please try again with a different account
+                    </span>
+                  </motion.p>
+                ) : (
+                  <motion.p
+                    key="normal-login"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.25, ease: "easeInOut" }}
+                    className="text-4xl font-bold text-center">
+                    Mini-app Administration Page
+                    <br />
+                    <span className="text-lg opacity-50 -mt-2">
+                      Connect your Base account to access the admin functions
+                    </span>
+                  </motion.p>
+                )}
+              </AnimatePresence>
+            </div>
+
+            <div className="w-fit mt-3">
+              <SignInWithBaseButton
+                align="center"
+                variant="solid"
+                colorScheme="light"
+                onClick={signInWithBase}
+              />
+            </div>
           </div>
         </motion.div>
       )}
